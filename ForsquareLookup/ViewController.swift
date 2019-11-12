877a5f56a2d0c01f8754666526a1e04f568a3fbc
Patryk Winowski <patryk.winowski@jeppesen.com>
2019-11-12 23:24:05 +0100

    Initial Commit

---
+++ ForsquareLookup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForsquareLookup
+//
+//  Created by Patryk Winowski on 12/11/2019.
+//  Copyright © 2019 Patryk Winowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ForsquareLookupTests/ForsquareLookupTests.swift
@@ -0,0 +1,34 @@
+//
+//  ForsquareLookupTests.swift
+//  ForsquareLookupTests
+//
+//  Created by Patryk Winowski on 12/11/2019.
+//  Copyright © 2019 Patryk Winowski. All rights reserved.
+//
+
+import XCTest
+@testable import ForsquareLookup
+
+class ForsquareLookupTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
